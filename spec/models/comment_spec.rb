0320352ba5e92484145654b9ea2fad8508f584a2
Degisew <jegisew21@gmail.com>
2023-01-28 19:09:53 +0300

    added unit tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'validation of custom methods' do
+    user2 = User.create!(name: 'Dagi', photo: 'https://example.com', bio: 'Software enginner', posts_counter: 0)
+    post1 = Post.create!(user_id: user2.id, title: 'RoR', text: 'Hello rails intro', comments_counter: 0,
+                         likes_counter: 0)
+    comment = Comment.create!(user_id: user2.id, post_id: post1.id)
+    it 'likes_counter_updater should return 1' do
+      counter = comment.comments_counter_updater.comments_counter
+      expect(counter).to be 1
+    end
+  end
+end
