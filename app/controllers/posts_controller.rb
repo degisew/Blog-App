2ee2d42b7c2ef7fa67b12c1dcf3e56de0ec9189b
Degisew <jegisew21@gmail.com>
2023-02-03 15:17:24 +0300

    fix routing issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = User.includes(:posts)
+    @user = User.find_by(id: params[:user_id])
   end
 
   def show
-    @post = Post.find_by(params[:id])
+    @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: params[:user_id])
   end
 end
